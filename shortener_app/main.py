9c00af80f866b782eed495afc902906cf1509369
matthewgouyer <mgouyer@live.com>
2025-04-25 15:58:32 -0700

    Basic API endpoint setup

--- shortener_app/main.py
+++ shortener_app/main.py
@@ -1,8 +1,21 @@
-from fastapi import FastAPI
+import validators
+from fastapi import FastAPI, HTTPException
 
+from . import schemas
 app = FastAPI()
 
 
 @app.get("/")
 def read_root():
-    return "Welcome to the URL Shortener API!"
+    return "Welcome to URLchemy!"
+
+def raise_bad_request(message):
+    raise HTTPException(status_code=400, detail=message)
+
+@app.post("/url")
+def create_url(url: schemas.URLBase):
+    if not validators.url(url.target_url):
+        raise_bad_request("Invalid URL")
+    # TODO: Here we need to save the URL to our database
+    return f"Create data base entry for: {url.target_url}"
+
