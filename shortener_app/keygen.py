a4eb3882a0805eda9ebe160c2876d0696420ae33
matthewgouyer <mgouyer@live.com>
2025-04-26 12:15:51 -0700

    Cleanup of API functions/endpoints

---
+++ shortener_app/keygen.py
@@ -0,0 +1,19 @@
+import secrets
+import string
+
+from sqlalchemy.orm import Session
+
+from . import crud
+
+# generate a random key of given length instead of hard code in main.py
+def create_random_key(length: int = 5) -> str:
+    chars = string.ascii_uppercase + string.digits
+    return "".join(secrets.choice(chars) for _ in range(length))
+
+
+def create_unique_random_key(db: Session) -> str:
+    key = create_random_key()
+    # check if key already exists in db
+    while crud.get_db_url_by_key(db, key):
+        key = create_random_key()
+    return key
--- shortener_app/main.py
+++ shortener_app/main.py
@@ -1,11 +1,9 @@
-import secrets
-
 import validators
 from fastapi import Depends, FastAPI, HTTPException, Request
 from fastapi.responses import RedirectResponse
 from sqlalchemy.orm import Session
 
-from . import models, schemas
+from . import crud, models, schemas
 from .database import SessionLocal, engine
 app = FastAPI()
 models.Base.metadata.create_all(bind=engine)
@@ -30,21 +28,13 @@ def raise_not_found(request):
     raise HTTPException(status_code=404, detail=message)
 
 @app.post("/url", response_model=schemas.URLInfo)
-def create_url(url: schemas.URLBase, db: Session = Depends(get_db)): # establish a db session
+def create_url(url: schemas.URLBase, db: Session = Depends(get_db)):
     if not validators.url(url.target_url):
         raise_bad_request(message="Your provided URL is not valid")
 
-    chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-    key = "".join(secrets.choice(chars) for _ in range(5))
-    secret_key = "".join(secrets.choice(chars) for _ in range(8))
-    db_url = models.URL(
-        target_url=url.target_url, key=key, secret_key=secret_key
-    )
-    db.add(db_url)
-    db.commit()
-    db.refresh(db_url)
-    db_url.url = key
-    db_url.admin_url = secret_key
+    db_url = crud.create_db_url(db=db, url=url)
+    db_url.url = db_url.key
+    db_url.admin_url = db_url.secret_key
 
     return db_url
 
@@ -53,13 +43,14 @@ def create_url(url: schemas.URLBase, db: Session = Depends(get_db)): # establish
 def forward_to_target_url(
     url_key: str, request: Request, db: Session = Depends(get_db)
 ):
-    # Query DB for URL w/ given key
+    # query DB for URL w/ given key
     db_url = (
         db.query(models.URL)
         .filter(models.URL.key == url_key, models.URL.is_active)
         .first()
     )
-    if db_url:
+    # walrus operator for matching url found
+    if db_url := crud.get_db_url_by_key(db=db, url_key=url_key):
         # redirect to target url
         return RedirectResponse(db_url.target_url)
     else:
