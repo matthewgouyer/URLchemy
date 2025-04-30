6b2079533ded70e2b83634a5dbe2fe5e570ddccf
matthewgouyer <mgouyer@live.com>
2025-04-30 14:16:27 -0700

    Refactoring key type instead of two seperate functions

--- backend/crud.py
+++ backend/crud.py
@@ -18,20 +18,12 @@ def create_db_url(db: Session, url: schemas.URLBase, title: str, description: st
     db.refresh(db_url)
     return db_url
 
-# check db for active db entries provided w/ non secret key
-def get_db_url_by_key(db: Session, url_key: str) -> models.URL:
+# check db for active db entries provided w/ key type
+def get_url_by_key_type(db: Session, key: str, key_type: str) -> models.URL:
+    filter_condition = models.URL.key if key_type == "key" else models.URL.secret_key
     return (
-        db.query(models.URL)
-        .filter(models.URL.key == url_key, models.URL.is_active)
-        .first()
-    )
-
-# check db for active db entries provided w/ secret key
-def get_db_url_by_secret_key(db: Session, secret_key: str) -> models.URL:
-    return (
-        db.query(models.URL)
-        .filter(models.URL.secret_key == secret_key, models.URL.is_active)
-        .first()
+        # query db for key type
+        db.query(models.URL).filter(filter_condition == key, models.URL.is_active).first()
     )
 
 # db click count for shortened url
@@ -54,4 +46,4 @@ def deactivate_db_url_by_secret_key(
 
 # query unique urls for db viz
 def get_unique_urls(db: Session):
-    return db.query(URL.target_url).distinct().filter(URL.is_active == True).all()
+    return db.query(URL.target_url).distinct().filter(URL.is_active).all()
