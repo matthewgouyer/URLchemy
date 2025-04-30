849de27e1dd7a5fddff2c508519ffcaf7925bc5c
matthewgouyer <mgouyer@live.com>
2025-04-30 14:32:22 -0700

    Pydoc for endpoints

--- backend/main.py
+++ backend/main.py
@@ -9,9 +9,9 @@ from .database import SessionLocal, engine
 from .config import get_settings
 from .scraper import scrape_metadata
 
-
 app = FastAPI()
 models.Base.metadata.create_all(bind=engine)
+
 
 # dependency to get database session going and close in case of errors
 def get_db():
@@ -20,6 +20,7 @@ def get_db():
         yield db
     finally:
         db.close()
+
 
 # public info -> shortened URL
 def get_public_info(db_url: models.URL) -> schemas.URLInfo:
@@ -33,6 +34,7 @@ def get_public_info(db_url: models.URL) -> schemas.URLInfo:
         clicks=db_url.clicks,
     )
 
+
 # admin info (public info + secret keys) -> shortened URL
 def get_admin_info(db_url: models.URL) -> schemas.URLAdminInfo:
     base_url = URL(get_settings().base_url)
@@ -40,16 +42,19 @@ def get_admin_info(db_url: models.URL) -> schemas.URLAdminInfo:
         "administration info", secret_key=db_url.secret_key
     )
     return schemas.URLAdminInfo(
-        **get_public_info(db_url).model_dump(), # using model_dump from pydantic
+        **get_public_info(db_url).model_dump(),  # using model_dump from pydantic
         admin_url=str(base_url.replace(path=admin_endpoint)),
     )
+
 
 @app.get("/")
 def read_root():
     return "Welcome to URLchemy!"
 
+
 def raise_bad_request(message):
     raise HTTPException(status_code=400, detail=message)
+
 
 def raise_not_found(request):
     message = f"Page '{request.url}' Not Found"
@@ -58,6 +63,15 @@ def raise_not_found(request):
 
 @app.post("/url")
 def create_url(url: schemas.URLBase, db: Session = Depends(get_db)):
+    """
+    Create a shortened URL for the given target URL.
+    Also scrapes metadata from the target URL and returns a list of unique URLs.
+
+    :param url (schemas.URLBase): The target URL to be shortened.
+    :param db (Session): The database session.
+    :return: (dict) A dictionary containing the shortened URL and a list of unique URLs.
+    :raises HTTPException: If the provided URL is invalid.
+    """
     if not validators.url(url.target_url):
         raise HTTPException(status_code=400, detail="Invalid URL provided")
 
@@ -66,7 +80,7 @@ def create_url(url: schemas.URLBase, db: Session = Depends(get_db)):
         db=db,
         url=url,
         title=metadata.get("title", "No title found"),
-        description=metadata.get("description", "No description found")
+        description=metadata.get("description", "No description found"),
     )
 
     # spit out all unique URLs
@@ -74,22 +88,33 @@ def create_url(url: schemas.URLBase, db: Session = Depends(get_db)):
 
     return {
         "shortened_url": get_public_info(db_url),
-        "unique_urls": [url[0] for url in unique_urls]
+        "unique_urls": [url[0] for url in unique_urls],
     }
+
 
 # call for requested url to point to host and key pattern
 @app.get("/{url_key}")
 def forward_to_target_url(
-    url_key: str, request: Request, db: Session = Depends(get_db)
+        url_key: str, request: Request, db: Session = Depends(get_db)
 ):
+    """
+    Redirect to the target URL associated with the given shortened URL key.
+
+    :param url_key (str): The key of the shortened URL.
+    :param request (Request): The incoming HTTP request.
+    :param db (Session): The database session.
+    :return: (RedirectResponse) Redirects to the target URL.
+    :raises HTTPException: If the URL key is not found or inactive.
+    """
     # walrus operator for matching url found
     if db_url := crud.get_db_url_by_key(db=db, url_key=url_key):
-        crud.update_db_clicks(db=db, db_url=db_url) # increment click count
+        crud.update_db_clicks(db=db, db_url=db_url)  # increment click count
         # redirect to target url
         return RedirectResponse(db_url.target_url)
     else:
         # raise 404 error if key is not found or inactive
         raise_not_found(request)
+
 
 # call for requested url to point to host and secret key pattern
 @app.get(
@@ -97,31 +122,47 @@ def forward_to_target_url(
     name="administration info",
     response_model=schemas.URLAdminInfo,
 )
-def get_url_info(
-    secret_key: str, request: Request, db: Session = Depends(get_db)
-):
+def get_url_info(secret_key: str, request: Request, db: Session = Depends(get_db)):
+    """
+    Retrieve administrative information for a shortened URL using the secret key.
+
+    :param secret_key (str): The secret key of the shortened URL.
+    :param request (Request): The incoming HTTP request.
+    :param db (Session): The database session.
+    :return: (schemas.URLAdminInfo) Administrative information about the shortened URL.
+    :raises HTTPException: If the secret key is not found or inactive.
+    """
     if db_url := crud.get_db_url_by_secret_key(db, secret_key=secret_key):
         return get_admin_info(db_url)
     else:
         raise_not_found(request)
 
+
 # endpoint to deactivate shortened url using secret key
 @app.delete("/admin/{secret_key}")
-def delete_url(
-    secret_key: str, request: Request, db: Session = Depends(get_db)
-):
+def delete_url(secret_key: str, request: Request, db: Session = Depends(get_db)):
+    """
+    Deactivate a shortened URL using the secret key.
+
+    :param secret_key (str): The secret key of the shortened URL.
+    :param request (Request): The incoming HTTP request.
+    :param db (Session): The database session.
+    :return: (dict) A message confirming the deletion of the shortened URL.
+    :raises HTTPException: If the secret key is not found or already deactivated.
+    """
     if db_url := crud.deactivate_db_url_by_secret_key(db, secret_key=secret_key):
         message = f"Successfully deleted shortened URL for '{db_url.target_url}'"
         return {"detail": message}
     else:
-        raise_not_found(request) # raise 404 error if key is not found or deactivated
+        raise_not_found(request)  # raise 404 error if key is not found or deactivated
 
-'''
+
+"""
 
 # endpoint to get unique URLs list for db viz
 @app.get("/unique-urls")
 def get_unique_urls_endpoint(db: Session = Depends(get_db)):
     unique_urls = crud.get_unique_urls(db)
     return {"unique_urls": [url[0] for url in unique_urls]}
-    
-'''
+
+"""
